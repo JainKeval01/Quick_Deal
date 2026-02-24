95553292637e05fcbfb32767b317f0e2a352ccf9
Keval <jainkeval.29805@gmail.com>
2026-02-24 16:44:59 +0530

    Little modification in the signUp files

--- app/src/main/java/com/example/quickdeal/activities/Login_Page.java
+++ app/src/main/java/com/example/quickdeal/activities/Login_Page.java
@@ -7,6 +7,9 @@ import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 import com.example.quickdeal.databinding.ActivityLoginPageBinding;
 
@@ -21,7 +24,11 @@ public class Login_Page extends AppCompatActivity {
         EdgeToEdge.enable(this);
         loginBinding = ActivityLoginPageBinding.inflate(getLayoutInflater());
         setContentView(loginBinding.getRoot());
-
+        ViewCompat.setOnApplyWindowInsetsListener(loginBinding.getRoot(), (v, windowInsets) -> {
+            Insets insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(insets.left, insets.top, insets.right, insets.bottom);
+            return WindowInsetsCompat.CONSUMED;
+        });
         loginBinding.loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -40,6 +47,14 @@ public class Login_Page extends AppCompatActivity {
                     startActivity(userIntent);
                     finish();
                 }
+            }
+        });
+
+        loginBinding.signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent userIntent = new Intent(getApplicationContext(), Signup_page.class);
+                startActivity(userIntent);
             }
         });
     }
--- app/src/main/java/com/example/quickdeal/activities/Signup_page.java
+++ app/src/main/java/com/example/quickdeal/activities/Signup_page.java
@@ -1,5 +1,6 @@
 package com.example.quickdeal.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -9,18 +10,31 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.quickdeal.R;
+import com.example.quickdeal.databinding.ActivitySignupPageBinding;
 
 public class Signup_page extends AppCompatActivity {
-
+    ActivitySignupPageBinding sBinding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_signup_page);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        sBinding = ActivitySignupPageBinding.inflate(getLayoutInflater());
+        setContentView(sBinding.getRoot());
+
+        ViewCompat.setOnApplyWindowInsetsListener(sBinding.getRoot(), (v, windowInsets) -> {
+            Insets insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(insets.left, insets.top, insets.right, insets.bottom);
+            return WindowInsetsCompat.CONSUMED;
         });
+
+        sBinding.back.setOnClickListener(v -> {
+            finish();
+        });
+
+        sBinding.createaccount.setOnClickListener(v -> {
+            Intent userIntent = new Intent(getApplicationContext(), TreeActivity.class);
+            startActivity(userIntent);
+        });
+
     }
 }
